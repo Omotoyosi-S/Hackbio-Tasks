ed154cc2de6de71db736d364f15ef1d603140492
Omotoyosi <104782943+Omotoyosi-S@users.noreply.github.com>
2022-06-11 10:49:38 +0100

    Update StageOne.sh

--- StageOne.sh
+++ StageOne.sh
@@ -8,3 +8,38 @@ grep -v "^>" DNA.fa | wc -c
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sha256sum Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
+
+#Installing softwares Fastqc, fastp and multiqc
+sudo apt-get install fastqc
+sudo apt-get install fastp
+sudo apt-get install multiqc
+
+#Downloading datasets
+mkdir RAWREADS && cd RAWREADS
+wget https://github.com/josoga2/yt-dataset/blob/main/dataset/raw_reads/ACBarrie_R1.fastq.gz?raw=true -O ACBarrie_R1.fastq.gz
+wget https://github.com/josoga2/yt-dataset/blob/main/dataset/raw_reads/ACBarrie_R2.fastq.gz?raw=true -O ACBarrie_R2.fastq.gz
+wget https://github.com/josoga2/yt-dataset/blob/main/dataset/raw_reads/Alsen_R1.fastq.gz?raw=true -O Alsen_R1.fastq.gz
+wget https://github.com/josoga2/yt-dataset/blob/main/dataset/raw_reads/Alsen_R2.fastq.gz?raw=true -O Alsen_R2.fastq.gz
+
+#creating ouput folder
+mkdir output
+
+#Implementing fastqc
+fastqc ACBarrie_R1.fastq.gz -o output/
+fastqc ACBarrie_R2.fastq.gz -o output/
+fastqc Alsen_R1.fastq.gz -o output/
+fastqc Alsen_R2.fastq.gz -o output/
+
+#Implementing fastp
+nano trim.sh
+bash trim.sh
+ls
+mv qc_reads/ trimmedreads
+cd trimmedreads/
+ls
+cd ..
+
+#Merging quality control reads using multiqc
+multiqc output/
+ls
+history
