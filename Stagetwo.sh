6ca6f70d206f4056414416c43dca48464bb799f7
Omotoyosi <104782943+Omotoyosi-S@users.noreply.github.com>
2022-06-15 09:32:28 +0100

    Create Stagetwo.sh

---
+++ Stagetwo.sh
@@ -0,0 +1,11 @@
+#!usr/bin/bash
+
+#Commands for downloading data from zenodo 
+mkdir raw_data && cd raw_data
+wget https://zenodo.org/record/2582555/files/SLGFSK-N_231335_r1_chr5_12_17.fastq.gz
+wget https://zenodo.org/record/2582555/files/SLGFSK-N_231335_r2_chr5_12_17.fastq.gz
+wget https://zenodo.org/record/2582555/files/SLGFSK-T_231336_r1_chr5_12_17.fastq.gz
+wget https://zenodo.org/record/2582555/files/SLGFSK-T_231336_r2_chr5_12_17.fastq.gz	
+
+#Downloading reference sequence
+wget https://zenodo.org/record/2582555/files/hg19.chr5_12_17.fa.gz
