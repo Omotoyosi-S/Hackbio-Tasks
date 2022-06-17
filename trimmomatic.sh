e5765744e204c7175eec921b4c6d6b0291b90e35
Omotoyosi <104782943+Omotoyosi-S@users.noreply.github.com>
2022-06-17 11:05:32 +0100

    Create trimmomatic.sh

---
+++ trimmomatic.sh
@@ -0,0 +1,14 @@
+#!usr/bin/bash
+for sample in `cat listtrim.txt`
+do
+       trimmomatic PE -threads 8 ${sample}_r1_chr5_12_17.fastq.gz ${sample}_r2_chr5_12_17.fastq.gz \
+               trimmed_reads/${sample}_r1_paired.fq.gz trimmed_reads/${sample}_r1_unpaired.fq.gz \
+               trimmed_reads/${sample}_r2_paired.fq.gz trimmed_reads/${sample}_r2_unpaired.fq.gz \
+               ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:8:keepBothReads \
+               LEADING:3 TRAILING:10 MINLEN:25
+
+       fastqc  trimmed_reads/${sample}_r1_paired.fq.gz  trimmed_reads/${sample}_r2_paired.fq.gz \
+                 -o trimmed_reads/Fastqc_results
+done
+
+multiqc  trimmed_reads/Fastqc_results  -o trimmed_reads/Fastqc_results
