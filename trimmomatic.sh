878be4e397c106fb798c1a1e11e114d7b0060e1d
Omotoyosi <104782943+Omotoyosi-S@users.noreply.github.com>
2022-06-17 12:15:56 +0100

    Update trimmomatic.sh

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -4,7 +4,7 @@ do
        trimmomatic PE -threads 8 ${sample}_r1_chr5_12_17.fastq.gz ${sample}_r2_chr5_12_17.fastq.gz \
                trimmed_reads/${sample}_r1_paired.fq.gz trimmed_reads/${sample}_r1_unpaired.fq.gz \
                trimmed_reads/${sample}_r2_paired.fq.gz trimmed_reads/${sample}_r2_unpaired.fq.gz \
-               ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:8:keepBothReads \
+               ILLUMINACLIP:/home/einstein/sample_tut/TruSeq3-PE.fa:2:30:10:8:keepBothReads \
                LEADING:3 TRAILING:10 MINLEN:25
 
        fastqc  trimmed_reads/${sample}_r1_paired.fq.gz  trimmed_reads/${sample}_r2_paired.fq.gz \
