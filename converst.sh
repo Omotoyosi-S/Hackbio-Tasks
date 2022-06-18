b182045c6d41d349833ae676eeda5a6681d4e680
Omotoyosi <104782943+Omotoyosi-S@users.noreply.github.com>
2022-06-18 18:06:13 +0100

    Update converst.sh

--- converst.sh
+++ converst.sh
@@ -1,9 +1,8 @@
 #!usr/bin/bash
 for sample in `cat mappinglist.txt`
 do
-        Convert SAM to BAM and sort it
+
         samtools view -@ 20 -S -b /home/einstein/crick/Saba/raw_data/ref/Mapping/${sample}.sam | samtools sort -@ 32 > Mapping/${sample}.sorted.bam
 
-        Index BAM file
         samtools index /home/einstein/crick/Saba/raw_data/ref/Mapping/${sample}.sorted.bam
 done
