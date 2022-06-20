bf3deb6997e256e340f0559a1bdcb5201930a764
Omotoyosi <104782943+Omotoyosi-S@users.noreply.github.com>
2022-06-20 09:54:14 +0100

    Create convert2pileup.sh

---
+++ convert2pileup.sh
@@ -0,0 +1,6 @@
+#!usr/bin/bash
+for sample in `cat mappinglist.txt`
+do
+        samtools mpileup -f hg19.chr5_12_17.fa Mapping/${sample}.refilter.bam --min-MQ 1 --min-BQ 28 \
+                > Variants/${sample}.pileup
+done
