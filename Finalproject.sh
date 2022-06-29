dd09e3ea34a439081d1cc0db1db4d346ba741344
Omotoyosi <104782943+Omotoyosi-S@users.noreply.github.com>
2022-06-29 19:41:32 +0100

    Create Finalproject.sh

---
+++ Finalproject.sh
@@ -0,0 +1,7 @@
+#!Usr/bin/bash
+cd project3
+mkdir Saba && cd Saba
+mkdir Dataset
+
+#Downloading dataset
+wget https://github.com/HackBio-Internship/public_datasets/blob/14972eaf11c4c172fdcd5e686da02a720655cca7/Asia_HLA_Distribution/complete_1000_genomes_sample_list_.tsv
